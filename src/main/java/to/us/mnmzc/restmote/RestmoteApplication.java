9e12b7a1086d18d84212179016780fe15eb81a5f
Dylan McCormick <mccormick.dy@northeastern.edu>
2026-03-03 17:41:25 -0500

    Initial commit

---
+++ src/main/java/to/us/mnmzc/restmote/RestmoteApplication.java
@@ -0,0 +1,13 @@
+package to.us.mnmzc.restmote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestmoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestmoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/to/us/mnmzc/restmote/RestmoteApplicationTests.java
@@ -0,0 +1,13 @@
+package to.us.mnmzc.restmote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestmoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
